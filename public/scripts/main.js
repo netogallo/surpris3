b541671ccd5f7d3bc7bb9d146457f610705eab27
Dominik Kundel <dominik.kundel@me.com>
2012-11-11 10:46:08 +0000

    include callback 1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -60,7 +60,7 @@ var surpris3 = {
 
 			$.ajax("/upload_picture", {type:"POST", data: {arg:rqst_str}, success: function(rsp){
 				if(!rsp.error){
-					FB.api('/me/surprise_photo:photobomb', 'post', { picture : (SERVER + '/photobomb/' + rsp.result._id.toString())});
+					FB.api('/me/surprise_photo:photobomb', 'post', { picture : (SERVER + '/photobomb/' + rsp.result._id.toString())}, function(rsp){});
 					$("#pb_upload").modal('hide');
 					$("#inputName").val("");
 					$("#inputURL").val("");
